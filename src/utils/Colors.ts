26a9a938dcd7c8fd1c186775a7971e36a1ab3721
Büşra Arıgün <busrarigun@gmail.com>
2024-05-12 18:20:30 +0300

    some resource changes

--- src/utils/Colors.ts
+++ src/utils/Colors.ts
@@ -1,10 +1,3 @@
 export const Colors = {
-    black:"#000",
-    white:"#fff",
-    purple:"#9575CD",
-    grey:"#E5E7E9",
-    darkGrey:"#676666",
-    yellow:"#FFD922",
-    red:'#F92247',
-    lightGrey:'#CFD8DC'
+    
 }
