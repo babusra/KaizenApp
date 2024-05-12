9c15837fbcc452dd28fbd177af87f6e58dc3a07b
Büşra Arıgün <busrarigun@gmail.com>
2024-05-12 18:21:31 +0300

    HomeScreen created

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,11 +1,16 @@
 import {View, Text} from 'react-native';
 import React, {FC} from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import HomeHeader from '../../components/HomeComponents/HomeHeader';
+import TagList from '../../components/HomeComponents/TagList';
 
 const HomeScreen: FC = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <HomeHeader/>
+        <TagList/>
+
+    </SafeAreaView>
   );
 };
 
